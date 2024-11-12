6f4ed7d30967abbe6b9d425752d842bdc0a65c79
Gabriel Belouze <gabriel@belouze.com>
2024-11-12 18:19:26 +0100

    Bump version -> 0.0.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,8 +9,8 @@
 project = "GeoBbox"
 copyright = "2024, Gabriel Belouze"
 author = "Gabriel Belouze"
-release = "0.0.2"
-version = "0.0.2"
+release = "0.0.3"
+version = "0.0.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
