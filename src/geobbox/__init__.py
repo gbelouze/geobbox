5db2e0e5767efa1f7124936e7d4578b57cac87d3
Gabriel Belouze <gabriel@belouze.com>
2026-01-15 13:18:46 +0100

    Add split utilities

--- src/geobbox/__init__.py
+++ src/geobbox/__init__.py
@@ -8,6 +8,7 @@ A python library for georeferenced bounding boxes.
 __version__ = "0.1.0"
 
 from .geobbox import GeoBoundingBox
+from .split import approximate_split, overlapping_split
 from .utm import UTM
 
-__all__ = ["GeoBoundingBox", "UTM"]
+__all__ = ["GeoBoundingBox", "UTM", "approximate_split", "overlapping_split"]
